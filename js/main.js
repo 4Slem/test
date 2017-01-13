51abf4ddde268f5e15c989378490c92c77a95d76
roma <4Slem@ukr.net>
2017-01-13 14:51:02 +0200

    tesst

--- js/main.js
+++ js/main.js
@@ -2,6 +2,8 @@
 	function fun () {
 		var mainElem = document.getElementsByClassName('main')[0];
 		mainElem.style.marginTop = -mainElem.offsetHeight/2 + 'px';
+		var body = document.getElementsByTagName('body')[0];
+		body.heigth = mainElem.offsetHeight;
 	}
 	window.onload = function () {
 		fun();
